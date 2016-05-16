10d48c769f952db648744a9780dee9340082ad3f
michalp334 <michal.porwisz@gmail.com>
2016-05-16 05:00:19 +0200

    Update Task.scala

--- Planner/src/Task.scala
+++ Planner/src/Task.scala
@@ -4,7 +4,7 @@ class Task {
   var name:String=""
   var interval:Option[Interval]=None
     
-  def modifyInterval(start:String, end:String) = interval = new Some(new Interval(start,end))  //czy takie zagnie¿dzenie "new" jest ok?
+  def modifyInterval(start:String, end:String) = interval = new Some(new Interval(start,end))  
    // in case interval wasn't specified by the user 
   }
 
